cc6a0e8b77bc0188cea55b1740b722ebba1c43cf
PabloAimar <pablo.aimar@kdsoft.io>
2020-09-08 20:04:59 -0300

    adivinanza terminada

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function Adivinanza(){
-    event.preventDefault
-    var entrada = document.getElementById('respuesta').value;
-    console.log(entrada)
-    if (entrada == "CoronaVirus")
-        respuesta = true
-    else 
-        respuesta = false
-        contador += 1
-    return contador
-}
 
-function ValidarIntentos(){
-    var intentos = Adivinanza()
-    var perdio = true;
-    if (intentos >= 4)
-        perdio = true
-    else
-        perdio = false
-}
+let letsPlay = () => {
+
+    let trys = 4;
+    const correctAnswer = 'coronavirus';
+    let correct = false;
+    let answer;
+    
+        while (trys > 0 && correct != true) {
+            trys--;
+            answer = prompt(" Tengo Corona, pero no soy Rey \n Ingrese la respuesta").toLowerCase();
+            if (answer === correctAnswer) {
+                alert( `Felicidades, respondisye correctamente!`);
+                correct = true;
+            } else if (trys == 3) {
+                alert(`Incorrecto, te quedan ${trys} intentos, pista: Estoy de Moda`);
+            } else if (trys == 2) {
+                alert(`Incorrecto, te quedan ${trys} intentos, pista: Soy muy Contagioso \n Ingrese la nueva respuesta:`);
+            } else if (trys == 1) {
+                alert(`Incorrecto, te queda ${trys} intento, pista: Mi nombre empieza con C  \n Ingrese la nueva respuesta`);
+            } else if (trys == 0) {
+               alert(`Perdiste! te quedaste sin intentos, la respuesta correcta era ${correctAnswer}`);
+            }
+        }        
+    }
